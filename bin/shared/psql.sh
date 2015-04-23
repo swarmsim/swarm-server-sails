e0ef39f0b8f741b94631013fe5496d65e64da115
Evan Rosson <github@swarmsim.com>
2015-04-22 18:34:14 -0700

    read psql password from env secrets, not pgpassword. remove pgpassword.

--- bin/shared/psql.sh
+++ bin/shared/psql.sh
@@ -2,4 +2,8 @@
 cd "`dirname "$0"`/../.."
 . ./bin/shared/shared.sh
 exportenv "$@"
-PGPASSFILE=secrets/pgpass psql -h $POSTGRES_HOST -p $POSTGRES_PORT -d "dbname=$POSTGRES_DB sslmode=verify-full sslrootcert=config/rds-combined-ca-bundle.pem" -U $POSTGRES_USER -w
+# http://www.postgresql.org/docs/8.4/static/libpq-envars.html
+# "Use of [the $PGPASS] environment variable is not recommended for security reasons (some operating systems allow non-root users to see process environment variables via ps);"
+# I'm already relying on environment variables for passwords/etc, so in this case we're already hosed. No plans to change this; my dev machine is secure. Careful developing on shared hosting.
+PGPASSWORD="$POSTGRES_PASSWORD" psql -h $POSTGRES_HOST -p $POSTGRES_PORT -d "dbname=$POSTGRES_DB sslmode=verify-full sslrootcert=config/rds-combined-ca-bundle.pem" -U $POSTGRES_USER -w
+
